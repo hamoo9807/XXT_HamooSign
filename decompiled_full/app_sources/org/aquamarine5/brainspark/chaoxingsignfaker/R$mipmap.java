be25d4c2b6f7c09f070ac7286576941ca32eaa3b
hamoo9807 <1341427526@qq.com>
2026-06-04 17:20:03 +0800

    删除一些参考文献

--- decompiled_full/app_sources/org/aquamarine5/brainspark/chaoxingsignfaker/R$mipmap.java
+++
@@ -1,3 +0,0 @@
-package org.aquamarine5.brainspark.chaoxingsignfaker;
-public final class R$mipmap {
-}
--- decompiled_full/app_sources/org/aquamarine5/brainspark/chaoxingsignfaker/datastore/easemob/MessageConfigOrBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.aquamarine5.brainspark.chaoxingsignfaker.datastore.easemob;
-public interface MessageConfigOrBuilder implements kl2 {
-
-    public abstract synthetic jl2 getDefaultInstanceForType();
-
-    public abstract boolean getField1();
-
-    public abstract synthetic boolean isInitialized();
-}
